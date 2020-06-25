52be887782e6cadb4e5a3c50f1fc82d54e89f5e6
Stephan Schmidt <stephan@hivomedia.de>
2020-06-25 18:00:13 +0200

    add genrelist basic functionality

--- src/components/GenreHistoryList.js
+++ src/components/GenreHistoryList.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
 
-export default function GenreHistoryList() {
+export default function GenreHistoryList(props) {
+    const { filteredListData } = props;
+    const match = useRouteMatch();
     return (
-        <div>
-            GenreHistory List !!
+        <div className="genrelist">
+            {filteredListData.map((data) => {
+                return (
+                    <Link key={data.id} to={`${match.url}/${data.id}`}>
+                        <div className="listitem"  style={{ margin: '50px' }}>
+                            <div className="listitem__name">{data.name}</div>
+                            <div className="listitem__description">{data.description}</div>
+                            <div className="listitem__decade">{data.decade}</div>
+                        </div>
+                    </Link>
+                )
+            })}
         </div>
     )
 }
