c580b88cd9804963c161fb9f411cee150f43a012
Stephan Schmidt <stephan@hivomedia.de>
2020-06-30 16:58:00 +0200

    added basic styling

--- src/components/GenreHistoryList.js
+++ src/components/GenreHistoryList.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link, useRouteMatch } from 'react-router-dom';
+import GenreHistorySingleListItem from './GenreHistorySingleListItem';
+import './GenreHistoryList.css';
 
 export default function GenreHistoryList(props) {
     const { filteredListData } = props;
@@ -8,12 +10,12 @@ export default function GenreHistoryList(props) {
         <div className="genrelist">
             {filteredListData.map((data) => {
                 return (
-                    <Link key={data.id} to={`${match.url}/${data.id}`}>
-                        <div className="listitem"  style={{ margin: '50px' }}>
-                            <div className="listitem__name">{data.name}</div>
-                            <div className="listitem__description">{data.description}</div>
-                            <div className="listitem__decade">{data.decade}</div>
-                        </div>
+                    <Link className="genrelist__link" key={data.id} to={`${match.url}/${data.id}`}>
+                        <GenreHistorySingleListItem 
+                            name={data.name}
+                            description={data.description}
+                            decade={data.decade}
+                        />
                     </Link>
                 )
             })}
