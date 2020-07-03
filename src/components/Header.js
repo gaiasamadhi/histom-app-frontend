b792826775de54ce20111eb4489738c3a9fb49ab
Manuel New <manuel.nitz83@gmail.com>
2020-07-03 18:53:51 +0200

    Added Decade-Menu and some nav styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
+import './header.css'
 
 export default function Header() {
     return (
-        <ul>
-            <li><Link exact to="/">Home</Link></li>
-            <li><Link to="/genrehistory">Genre-History</Link></li>
-            <li><Link to="/about">About</Link></li>
-        </ul>
+        <div className="genrehistory-header-container">
+            <ul>
+                <li><Link exact to="/">Home</Link></li>
+                <li><Link to="/genrehistory">Genre-History</Link></li>
+                <li><Link to="/about">About</Link></li>
+            </ul>
+            <h1>The histom app !!.'_'.!!</h1>
+        </div>
     )
 }
 
